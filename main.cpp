5f82479fdb573478e3709805d64fc22e22360593
Akash Shrivastva <akash.shrivastva@gmail.com>
2020-04-25 20:44:39 +0530

    added shortest path

--- main.cpp
+++ main.cpp
@@ -11,11 +11,15 @@ typedef long double ld;
 #define F first
 #define S second
 #define PII pair <ll,ll>
+#define PI pair<int,int>
 
 map<string, pll > m; // mapping sections to coordinates
 map<string, int > g; //mapping sections to groups 
 char a[31][31];
 vector <product> prod;
+int visited[31][31];
+map < pair< pair<int,int> , pair<int,int> >, string> finalPath;
+// string finalPath[31][31][31][31];
 
 void display()
 {
@@ -27,6 +31,66 @@ void display()
         cout<<endl;
     }
 }
+
+int check(int x, int y)
+{
+    if (x>0 and x<=30 and y>0 and y<=30)
+    {
+        if (a[x][y]==' ' and visited[x][y]==-1) return 1;
+        else return 0;
+    }
+    else return 0;
+}
+
+void bfs(PI source)
+{
+    queue< PI > q; 
+    q.push(source);
+    string path[31][31];
+    memset(visited,-1,sizeof(visited));
+    path[source.F][source.S] = "";
+
+    while(!q.empty()) {
+        int x = q.front().F, y = q.front().S;
+        visited[x][y]=1;
+        q.pop();
+        if (check(x, y + 1))
+            path[x][y+1] = path[x][y], path[x][y+1].pb('R'), q.push({x,y+1});
+        if (check(x, y - 1))
+            path[x][y-1] = path[x][y], path[x][y-1].pb('L'), q.push({x,y-1});
+        if (check(x - 1, y))
+            path[x-1][y] = path[x][y], path[x-1][y].pb('U'), q.push({x-1,y});
+        if (check(x + 1, y))
+            path[x+1][y] = path[x][y], path[x+1][y].pb('D'), q.push({x+1,y});
+    }
+
+    int i,j;
+    for (i=1;i<=30;i++)
+    {
+        for (j=1;j<=30;j++)
+        {
+            // finalPath[source.F][source.S][i][j] = path[i][j];
+            PI dest = {i,j};
+            finalPath[{source, {dest}}] = path[i][j];
+        }
+    }
+}
+
+void calculateShortestPath()
+{
+    int i,j;
+    // for (i=1;i<=30;i++)
+    // {
+    //     for (j=1;j<=30;j++)
+    //     {
+    //         if (a[i][j]==' ')
+    //             bfs({i,j});
+    //     }
+    // }
+    bfs({23,10});
+
+}
+
 int main()
 {
     ios_base::sync_with_stdio(false);
@@ -44,5 +108,7 @@ int main()
     for (i=0;i<=30;i++)
         for (j=0;j<=30;j++)
             a[i][j] = ob.a[i][j];
-    display();
+    
+    calculateShortestPath();
+    cout<<"hello world"<<endl;
 }
