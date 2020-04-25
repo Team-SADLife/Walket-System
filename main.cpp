7b58577b55774940a52a59311b087430f21c73ba
Akash Shrivastva <akash.shrivastva@gmail.com>
2020-04-26 00:39:35 +0530

    removed runtime error while cal. shortest path

--- main.cpp
+++ main.cpp
@@ -24,9 +24,9 @@ map < pair< pair<int,int> , pair<int,int> >, string> finalPath;
 void display()
 {
     ll i,j;
-    for (i=0;i<=30;i++)
+    for (i=1;i<=30;i++)
     {
-        for (j=0;j<=30;j++)
+        for (j=1;j<=30;j++)
             cout<<a[i][j]<<" ";
         cout<<endl;
     }
@@ -45,33 +45,36 @@ int check(int x, int y)
 void bfs(PI source)
 {
     queue< PI > q; 
-    q.push(source);
     string path[31][31];
+    q.push(source);
+    int i,j;
     memset(visited,-1,sizeof(visited));
     path[source.F][source.S] = "";
+    visited[source.F][source.S] = 1;
 
     while(!q.empty()) {
         int x = q.front().F, y = q.front().S;
-        visited[x][y]=1;
         q.pop();
         if (check(x, y + 1))
-            path[x][y+1] = path[x][y], path[x][y+1].pb('R'), q.push({x,y+1});
+            path[x][y+1] = path[x][y], path[x][y+1].pb('R'), q.push({x,y+1}), visited[x][y+1]=1;
         if (check(x, y - 1))
-            path[x][y-1] = path[x][y], path[x][y-1].pb('L'), q.push({x,y-1});
+            path[x][y-1] = path[x][y], path[x][y-1].pb('L'), q.push({x,y-1}), visited[x][y-1]=1;
         if (check(x - 1, y))
-            path[x-1][y] = path[x][y], path[x-1][y].pb('U'), q.push({x-1,y});
+            path[x-1][y] = path[x][y], path[x-1][y].pb('U'), q.push({x-1,y}), visited[x-1][y]=1;
         if (check(x + 1, y))
-            path[x+1][y] = path[x][y], path[x+1][y].pb('D'), q.push({x+1,y});
+            path[x+1][y] = path[x][y], path[x+1][y].pb('D'), q.push({x+1,y}), visited[x+1][y]=1;
     }
-
-    int i,j;
+    cout<<source.F<<" "<<source.S<<endl;
     for (i=1;i<=30;i++)
     {
         for (j=1;j<=30;j++)
         {
-            // finalPath[source.F][source.S][i][j] = path[i][j];
-            PI dest = {i,j};
-            finalPath[{source, {dest}}] = path[i][j];
+            if (a[i][j]==' ')
+            {
+                PI dest = {i,j};
+                finalPath[{source, {dest}}] = path[i][j];
+                // finalPath[source.F][source.S][i][j] = path[i][j];
+            }
         }
     }
 }
@@ -79,15 +82,15 @@ void bfs(PI source)
 void calculateShortestPath()
 {
     int i,j;
-    // for (i=1;i<=30;i++)
-    // {
-    //     for (j=1;j<=30;j++)
-    //     {
-    //         if (a[i][j]==' ')
-    //             bfs({i,j});
-    //     }
-    // }
-    bfs({23,10});
+    for (i=1;i<=30;i++)
+    {
+        for (j=1;j<=30;j++)
+        {
+            if (a[i][j]==' ')
+                bfs({i,j});
+        }
+    }
+    // bfs({1,1});
 
 }
 
